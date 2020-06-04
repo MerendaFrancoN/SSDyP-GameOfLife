de454c0b7bf11225ad65b59b4dad907784beffd7
MerendaFrancoN <merenda.franco83@gmail.com>
2020-06-04 05:35:38 -0300

    Updated

---
+++ cmake-build-debug/CMakeFiles/SSDyP_GameOfLife.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SSDyP_GameOfLife.dir/main.c.o"
+  "SSDyP_GameOfLife"
+  "SSDyP_GameOfLife.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SSDyP_GameOfLife.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
